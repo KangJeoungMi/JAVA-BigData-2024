0546118669653e1b4058a90be95e61ab21b9d3dc
KangJeoungMi <kbg973855@gmail.com>
2024-03-06 10:45:39 +0900

    upload

--- DAY09/p66_imageEditor.py
+++ DAY09/p66_imageEditor.py
@@ -3,6 +3,9 @@
 '''
 qrc 파일을 사용하려면
 > pyrcc5 "resources.qrc" -o "resources_rc.py"
+
+imutils 설치
+> pip install imutils
 '''
 
 import sys
@@ -11,10 +14,11 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtGui import QCloseEvent, QMouseEvent
 from PyQt5.QtWidgets import *
+import numpy as np
 # 리소스 파일 추가
 import resource_rc
-# openCv 추가
-import cv2
+# openCv,imutils 추가
+import cv2,imutils
 
 class WinApp(QMainWindow):   # QWidget이 아님!
     def __init__(self) -> None:
@@ -23,17 +27,17 @@ class WinApp(QMainWindow):   # QWidget이 아님!
         self.initSignal()
         
     def initUI(self):
-        uic.loadUi('./day09/pyNewPaint.ui', self)
-        self.setWindowIcon(QIcon('./day09/imgs/editor.png'))
-        self.setWindowTitle('이미지 에디터 v0.5')
+        # uic.loadUi('./day09/pyNewPaint.ui', self)   # VSCode 실행용
+        uic.loadUi('C:/Source/java-bigdata-2024/day09/pyNewPaint.ui', self)   # PyInstaller용
+        # self.setWindowIcon(QIcon('./day09/imgs/editor.png'))
+        self.setWindowIcon(QIcon('C:/Source/java-bigdata-2024/day09/imgs/editor.png'))
+        self.setWindowTitle('이미지에디터  v0.5')
         ## 이미지 추가 / 여러가지 UI에 대한 초기화
-        pixmap = QPixmap('./DAY09/imgs/panda.jpg').scaledToHeight(471)
+        # pixmap = QPixmap('./day09/ding.jpg').scaledToHeight(471)
+        pixmap = QPixmap('C:/Source/java-bigdata-2024/day09/imgs/panda.jpg').scaledToHeight(471)
         self.lblCanvas.setPixmap(pixmap)
-        
-        self.BColor = Qt.red # 빨강이 기본
-         
-        
-       # UI 초기화 끝 
+        self.brushColor = Qt.red    # 빨간색이 기본
+        ## UI 초기화 끝
         self.show()
         
     def initSignal(self):
@@ -51,7 +55,21 @@ class WinApp(QMainWindow):   # QWidget이 아님!
         
                 
     def actionGrayscaleClicked(self):
-        return
+        # temp.png 와 같은형태로 임시 이미지 저장
+        # opencv불러옴
+        # 그레이스케일로 변경한다음
+        # 변경한 이미지를 다시  pixmap으로 변환 뒤 lblcanvas에 올림
+        
+        # tempPath = './DAY09/imgs/save.png'
+        tempPath = 'C:/Source/java-bigdata-2024/day09/imgs/save.png'
+
+        pixmap = self.lblCanvas.pixmap()    # 라벨에 있는 그림을 pixmap 변수에 저장
+        pixmap.save(tempPath)
+        image = cv2.imread(tempPath)
+        grayImg = QImage(image, image.shape[1], image.shape[0], image.strides[0], QImage.Format_Grayscale16)
+        self.lblCanvas.setPixmap(QPixmap.fromImage(grayImg))
+    
+       
         
         
     def actionNewClicked(self):
